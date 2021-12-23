5d1e5e28b0e6a8de6851c124a4732b33442aaa75
Patrik Horvat <patrik.horvat@student.uniri.hr>
2021-12-23 15:22:41 +0100

    Fixed bugs in layout.js and logo.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
-import Navbar from "./Navbar/Navbar"
+import Navbar from "./Navbar/navbar"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
