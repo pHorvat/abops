259e9f3ab97b5231cdeca0016c1bc3892fc21ac9
Patrik Horvat <patrik.horvat@student.uniri.hr>
2021-12-23 10:55:16 +0100

    Dodan navbar Pokrenuti:  -gatsby clean  -npm install --save gatsby-plugin-styled-components styled-components babel-plugin-styled-components
    i dodati
     gatsby-plugin-styled-components
    u gatsby-config.js ako nije dodan

---
+++ src/components/Navbar/logo.js
@@ -0,0 +1,30 @@
+import React from "react"
+import styled from "styled-components"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+
+const LogoWrap = styled.div`
+  margin: auto 0;
+  flex: 0 1 36px;
+  @media (max-width: 768px) and (orientation: landscape) {
+    flex: 0 1 25px;
+  }
+`
+
+export function Logo() {
+    return (
+        <LogoWrap as={Link} to="/">
+        <StaticImage
+            src="../../images/gatsby-icon.png"
+            alt="logo"
+            placeholder="blurred"
+            layout="fixed"
+            width={50}
+        />
+        </LogoWrap>
+    )
+}
+
+
+
+export default Logo
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,16 +15,13 @@ const IndexPage = () => (
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
-      quality={95}
+      quality={50}
       formats={["auto", "webp", "avif"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
   </Layout>
 )
